0f9bd3c7e621a9a8b06edd195ce164e4cee7c10c
justnoted <just.noted3505@gmail.com>
2025-03-22 02:26:08 -0500

    Added a rotating discord status

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,9 +1,63 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
+const { MongoClient } = require('mongodb');
+const { mongoUri } = require('../../config.json');
+
+let statusList = [
+  {
+    type: ActivityType.Custom,
+    name: "cat melon",
+    state: ":3"
+  },
+  {
+    type: ActivityType.Custom,
+    name: "woah, i can do this?",
+    state: "i have a rotating status :D"
+  },
+  {
+    type: ActivityType.Custom,
+    name: "lucia noise complaints",
+    state: "balls"
+  },
+  {
+    type: ActivityType.Streaming,
+    name: "palo cereal.",
+    url: 'https://www.youtube.com/watch?v=pKVzd8k7VsA',
+    state: "best ad known to man"
+  },
+]
+
+async function connectToDB() {
+  const client = new MongoClient(mongoUri);
+
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 })
+    console.log("Successfully connected to MongoDB");
+  } catch (error) {
+    console.error(`(X) MongoDB Connection failed. Please review:\n${error}`);
+  }
+}
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
-  execute(client) {
+
+  async execute(client) {
+    // client.user.setPresence({
+    //   status: "online",
+    //   activities: [{
+    //     type: ActivityType.Custom,
+    //     state: "test state test lol"
+    //   }]
+    // })
+
+    let index = 0;
+    setInterval(() => {
+      client.user.setActivity(statusList[index]);
+      index = (index + 1) % statusList.length;
+    }, 15000)
+
+    await connectToDB()
     console.log(`${client.user.username} is now online!`);
   },
 };
