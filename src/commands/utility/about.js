78ac6cf9e5a8c3a7b2570efc67482a761a0be874
justnoted <just.noted3505@gmail.com>
2025-03-28 17:34:04 -0500

    Added utility commands

--- src/commands/utility/about.js
+++ src/commands/utility/about.js
@@ -1,17 +1,15 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
 
-module.exports = {
-  data: new SlashCommandBuilder()
-      .setName('about')
-      .setDescription('View why this bot is a thing'),
+export const data = new SlashCommandBuilder()
+  .setName('about')
+  .setDescription('View why this bot is a thing');
+  
+export async function execute(interaction) {
+  const embed = new EmbedBuilder()
+    .setColor(0xb46d3e)
+    .setAuthor({ name: "About Ballocal", iconURL: interaction.client.user.displayAvatarURL() })
+    .setDescription("Ballocal is a discord bot, tasked with keeping track of events for Ballistimite Paradise, or Ballodise, as it's often referred to :3")
+    .setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
 
-  async execute(interaction){
-    const embed = new EmbedBuilder()
-        .setColor(0xb46d3e)
-        .setAuthor({ name: "About Ballocal", iconURL: interaction.client.user.displayAvatarURL() })
-        .setDescription("Ballocal is a discord bot, tasked with keeping track of events for Ballistimite Paradise, or Ballodise, as it's often referred to :3")
-        .setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() })
-
-    interaction.reply({embeds: [embed]})
-  }
+  interaction.reply({ embeds: [embed] });
 }
--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -1,17 +1,15 @@
-const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
 
-module.exports = {
-  data: new SlashCommandBuilder()
-      .setName('help')
-      .setDescription('View all available commands'),
+export const data = new SlashCommandBuilder()
+  .setName('help')
+  .setDescription('View all available commands');
 
-  async execute(interaction){
-    const embed = new EmbedBuilder()
-        .setColor(0xb46d3e)
-        .setAuthor({name: "Ballocal's Help Guide", iconURL: interaction.client.user.displayAvatarURL()})
-        .addFields({name: "Utility", value: "- `/about` - View why this bot a thing\n- `/help` - You're viewing it right now\n- `/ping` - View latency"})
-        .setFooter({text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL()})
+export async function execute(interaction) {
+  const embed = new EmbedBuilder()
+    .setColor(0xb46d3e)
+    .setAuthor({ name: "Ballocal's Help Guide", iconURL: interaction.client.user.displayAvatarURL() })
+    .addFields({ name: "Utility", value: "- `/about` - View why this bot a thing\n- `/help` - You're viewing it right now\n- `/ping` - View latency" })
+    .setFooter({ text: `Requested by ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() });
 
-    interaction.reply({embeds: [embed]})
-  }
+  interaction.reply({ embeds: [embed] });
 }
